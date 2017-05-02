fd72f4a78fd177955d6bb11c1b420d669596fb93
Breno Leitao <breno.leitao@gmail.com>
2017-05-02 17:18:07 +0000

    Specific case for the VMX restore issue

---
+++ narrow/worker.c
@@ -0,0 +1,112 @@
+#include <inttypes.h>
+#include <htmintrin.h>
+#include <stdio.h>
+
+#define _ asm
+
+void worker()
+{
+ // VMX registers.
+ register vector __int128 vmx0  asm ("vs32");
+
+ vector __int128 vmx_correct_value[32];
+ vector __int128 vmx_scratch_area[2];
+
+ uint64_t offset = 0;
+ uint64_t z;
+
+ // Expected correct values restored after a HTM failure.
+ vmx_correct_value[0]  = (vector __int128) {0xBEEF};
+
+ // Expected correct value restored to VRSAVE after a HTM failure.
+ uint64_t vrsave_correct_value[1];     // A place to keep the correct value in memory
+ vrsave_correct_value[0] = 0xBABEBEEF; // The correct value itself, a 32-bit value
+
+ // Set values for each VMX register just before entering in a transactional block.
+ // Values below must match the expected correct values after a HTM failure,
+ // as specified in the code section above. TODO: add random generated value as
+ // the expected correct values to torture kernel VMX restore code.
+ vmx0  = (vector __int128) {0xBEEF};
+
+/***************
+ ** HTM BEGIN **
+****************/
+ _ ("tbegin.  \n\t");
+ _ goto ("beq %l[_failure] \n\t" : : : : _failure);
+
+/**************
+ ** HTM BODY **
+ **************/
+
+ // We set VMX register with a value different from what they
+ // were set *before* entering the HTM. If the transaction
+ // fails, then VMX registers after _failure is taken must
+ // be restored back to the values *before* entering the HTM.
+ vmx0  = (vector __int128) {0xBABE};
+
+
+ _ ("nop \n\t");
+/*************
+ *
+ ** HTM END **
+ *************/
+ _ ("tend.    \n\t");
+ _ goto ("b %l[_success] \n\t" : : : : _success);
+
+
+_failure:
+   _ goto (
+
+     // Check if VRSAVE is sane.
+     "lwz      5, 0(%[vrsave_correct_value]) \n\t"
+     "mfvrsave 6                             \n\t"
+     "cmpd     5, 6                          \n\t"
+     "li       6, 32                         \n\t"
+     "bne  %l[_value_mismatch]               \n\t"
+
+     // Check if vmx0 is sane.
+     "stvx 1, 0, %[vmx_scratch_area]   \n\t"
+     "lvx  1, 0, %[vmx_correct_value]  \n\t"
+     "vcmpequb. 0, 0, 1                \n\t"
+     "lvx  1, 0,  %[vmx_scratch_area]  \n\t"
+     "mfvrd 5, 0                       \n\t"
+     "li     6, 0                      \n\t"
+     "bc   4, 24, %l[_value_mismatch]  \n\t"
+
+     // Reach here, then all registers are sane. 
+     "b  %l[_value_match]              \n\t" 
+
+        : // no output
+        : [offset] "r"(offset),
+          [vmx_correct_value] "r"(vmx_correct_value),
+          [vmx_scratch_area]  "r"(vmx_scratch_area),
+          [vrsave_correct_value] "r"(vrsave_correct_value)
+        : "memory", "r5", "r6"
+        : _value_match, _value_mismatch
+     );
+
+_value_mismatch:
+	z = __builtin_get_texasr();
+	printf("\n\n==============\n\n");
+	printf("\nFailure with error: %lx\n\n",  _TEXASR_FAILURE_CODE(z));
+	printf(": Summary error: %lx\n",  _TEXASR_FAILURE_SUMMARY(z));
+	printf(": TFIAR error: %lx\n\n", _TEXASR_TFIAR_EXACT(z));
+
+       _ (".long 0"); // exit with a core dump
+
+_value_match:
+	printf("!");
+	goto _finish;
+
+_success:
+	printf(".");
+
+_finish:
+	_ ("nop"); // Can't label in the void...
+
+}
+
+int main(){
+	printf("Torture tool\n");
+	worker();
+}
