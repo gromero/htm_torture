b9f4ae955d478c33414b5445522b52ce5d135d57
Gustavo Romero <gromero@linux.vnet.ibm.com>
2017-09-15 16:52:09 -0400

    narrow/context_switch: improve run script

--- narrow/context_switch/run.sh
+++ narrow/context_switch/run.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
 ulimit -c unlimited
-gcc -O0 -g context_switch_.c -DMSR_FP -o context_switch_fp -pthread
+gcc -O0 -g context_switch_.c -D$MSR_FLAG -o context_switch_trial -pthread
 touch core
-while(true); do rm core; ./context_switch_fp &>/dev/null; gdb ./context_switch_fp ./core -q -ex 'print $vs0.uint128' -ex 'print $vs32.uint128'  -ex "quit"  |& egrep " = "; done
+while(true); do rm core; ./context_switch_trial &>/dev/null; gdb ./context_switch_trial ./core -q -ex 'print $vs0.uint128' -ex 'print $vs32.uint128'  -ex "quit"  |& egrep " = "; done
