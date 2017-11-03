4db9bb48569abb8ab5d318aab540f7dc03bb2454
Breno Leitao <breno.leitao@gmail.com>
2017-11-03 09:00:01 -0400

    threads: Removing unused function
    Removing a function that is not used anymore

--- src/threads.c
+++ src/threads.c
@@ -1,29 +1,10 @@
 #include "torture.h"
-
-void start_threads(uint64_t threads){
-	pthread_t thread[threads];
-
-#ifdef DEBUG
-	printf("Creating %"PRIu64" threads\n", threads);
-#endif
-
-	for (uint64_t i = 0; i < threads; i++)
-		pthread_create(&thread[i], NULL, &worker,  NULL);
-
-#ifdef DEBUG
-	printf("Waiting %"PRIu64" threads to finish\n", threads);
-#endif
-	for (int i = 0; i < threads; i++)
-		pthread_join(thread[i], NULL);
-}
-
 void init_workers(void) { num_used_threads=0; }
 
 void start_workers(uint64_t workload, uint64_t nr_workers)
 {
-  for (uint64_t i = 0; i < nr_workers && num_used_threads < THREADS; i++) {
+  for (uint64_t i = 0; i < nr_workers; i++) {
     pthread_create(&thread_pool[num_used_threads], NULL, &worker, (void *) workload);
-//  pthread_join(thread_pool[i], NULL);
     num_used_threads++;
   }
 }
